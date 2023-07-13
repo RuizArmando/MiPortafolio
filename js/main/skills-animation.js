95479b4b2269cf147b422b1e90ef6c7707f1152f
Armando Ruiz <ruiz12armando@gmail.com>
2023-07-13 12:31:11 -0600

    Nuevo archivo js de animacion

---
+++ js/main/skills-animation.js
@@ -0,0 +1,24 @@
+const nav1 = document.getElementById('skills__nav');
+const nav2 = document.getElementById('hobbies__nav');
+const nav3 = document.getElementById('academic__nav');
+
+const loginNav = (entradas, observador) => {
+
+	entradas.forEach((entrada) => {
+		if(entrada.isIntersecting){
+			entrada.target.classList.add('visible');
+		} else {
+			//entrada.target.classList.remove('visible');
+		}
+	});
+}
+
+const observador = new IntersectionObserver(loginNav, {
+	root: null,
+	rootMargin: '0px 0px 0px 0px',
+	threshold: 1.0
+});
+
+observador.observe(nav1);
+observador.observe(nav2);
+observador.observe(nav3);
