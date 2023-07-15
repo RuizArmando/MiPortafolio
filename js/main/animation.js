03bc17ab13e6ad148dc3d6ff6b3e6262186f3cf4
Armando Ruiz <ruiz12armando@gmail.com>
2023-07-14 18:39:27 -0600

    Cambio en la visualicacion

--- js/main/animation.js
+++ js/main/animation.js
@@ -1,6 +1,7 @@
 const nav1 = document.getElementById('skills__nav');
 const nav2 = document.getElementById('hobbies__nav');
 const nav3 = document.getElementById('academic__nav');
+const imagen1 = document.getElementById('proyect__div');
 
 const loginNav = (entradas, observador) => {
 
@@ -8,7 +9,7 @@ const loginNav = (entradas, observador) => {
 		if(entrada.isIntersecting){
 			entrada.target.classList.add('visible');
 		} else {
-			//entrada.target.classList.remove('visible');
+			entrada.target.classList.remove('visible');
 		}
 	});
 }
@@ -22,3 +23,4 @@ const observador = new IntersectionObserver(loginNav, {
 observador.observe(nav1);
 observador.observe(nav2);
 observador.observe(nav3);
+observador.observe(imagen1);
--- js/menu/menu.js
+++ js/menu/menu.js
@@ -1,11 +1,38 @@
 function MenuPosition(event, targetId){
     event.preventDefault()
-    let menu = document.querySelector('.menu')
-    let aboutSection = document.querySelector(targetId)
-    let aboutSectionTop = aboutSection.offsetTop - 50
-    menu.classList.add('adjusted')
+    let menu = document.querySelector('.menu');
+    let aboutSection = document.querySelector(targetId);
+    let aboutSectionTop = aboutSection.offsetTop - 50;
+    menu.classList.add('adjusted');
     window.scroll({
         top: aboutSectionTop,
         behavior: 'smooth'
-    })
+    });
+    targetId = targetId.substring(1);
+    Menu(targetId);
+}
+
+function Menu(sectionID){
+    let main = document.querySelectorAll('#mainHTML section');
+
+    main.forEach((section) =>{
+        section.style.display = 'none';
+    });
+
+    let id = document.getElementById(sectionID);
+    id.style.display = 'block';
+
+    linkBold(sectionID);
+}
+
+function linkBold(linkID){
+    let menuLink = document.querySelectorAll('#menu a');
+
+    menuLink.forEach((a) =>{
+        a.style.fontWeight = 'normal'
+    });
+    
+    linkID = "link__" + linkID;
+    let link = document.getElementById(linkID);
+    link.style.fontWeight = 'bold';
 }
